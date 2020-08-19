a7dd26ef20a96d6ea02fc73c385027b46afb2ccd
liguangpeng <liguangpeng@bnz.cn>
2020-08-19 13:58:52 +0800

    把本地工程添加到git

---
+++ git-test/src/com/bnz/Main.java
@@ -0,0 +1,13 @@
+package com.bnz;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
